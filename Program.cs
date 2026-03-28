a05a30dd61baf5fed77a4aea1412e47db634d960
Saint Dev <jqydjdj@gmail.com>
2026-03-27 19:21:40 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using ClinicaSalud.Models;
+using ClinicaSalud.Models;
 
 //var id = id;
 //string name = name;
@@ -7,47 +7,58 @@
 Console.Write(new Pacientes(123,"szapatar",12, "diarrea").Id);
 Console.Write(new Pacientes(123,"szapatar",12, "diarrea").Edad);
 Console.Write(new Pacientes(123,"szapatar",12, "diarrea").Sintoma);*/
-bool system = true;
+bool system = false;
 
 List<string> nombrePacientes = new List<string>();
-/*{
-   "Juan Sanchez\n","Camilo Cifuentes\n","Bad Bunny\n" 
-};*/
-//Console.WriteLine("ingresa el paciente 1");
 
 bool exit = false;
-while (system && !exit);
+bool repeat = false;
+while (system );
 
 {
-    string Options = "1.Register Patient\n"+
+    string Options =  "Please Enter The Number Do You Want\n" +
+                      "1.Register Patient\n"+
                      "2. List Patients\n" +
                      "3. Search Patients\n" +
-                     "4.Exit\n" +
-                     "Please Enter The First Letter Of The Option You Want to\n";
-    Console.Write(Options);
+                     "4.Exit\n";
+    Console.WriteLine(Options);
 //var User = Console.ReadLine();
 
     ConsoleKeyInfo choose = new ConsoleKeyInfo();
 
     choose = Console.ReadKey(true);
-    system = false;
-
 
 
     switch (choose.Key)
     {
-        case ConsoleKey.R:
+        case ConsoleKey.D1:
         {
-            Console.WriteLine("Do You Want Register A Patient And");
+            Console.WriteLine("Enter Data Of Patient");
             string paciente = Console.ReadLine();
+            ;
+            if (!string.IsNullOrEmpty(paciente))
+            {
+                Console.WriteLine(Options);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(Options);
+            }
             nombrePacientes.Add(paciente);
+            Console.Clear();
+            Console.WriteLine(Options);
+            
+            //if (paciente == "paciente")
+            //{
+              //  continue;
+            //}
+            //Console.WriteLine(Options);
             break;
         }
 
-    case ConsoleKey.L:
+        case ConsoleKey.D2:
         {
-            Console.Write("Do You Want List Patients And This Process Charging\n");
-            //Console.WriteLine(string.Join(" ", nombrePacientes));
             foreach (var pacientes in nombrePacientes)
             {
                 Console.WriteLine($"El paciente {pacientes}");
@@ -56,27 +67,21 @@ while (system && !exit);
             break;
         }
 
-        case ConsoleKey.S:
+        case ConsoleKey.D3:
         {
             Console.Write("Do You Want Search Any Patient And\n");
             break;
         }
 
-        case ConsoleKey.E:
+        case ConsoleKey.D4:
         {
             Console.Write("Do You Want Exit Of The System And\n");
             exit = true;
             break;
         }
-        default:
-        {
-            Console.Clear();
-            Console.WriteLine("Please Select A Valid Option\n");
-            system = true;
-            break;
-
-        }
+        
     }
+    
 }
 
 
