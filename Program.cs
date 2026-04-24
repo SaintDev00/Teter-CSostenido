9fa10e47fa33a0104bcc793927c308d3e8ad1f80
JJ-Saint <jqydjdj@gmail.com>
2026-04-23 20:45:03 -0500

    s

--- Program.cs
+++ Program.cs
@@ -1,77 +1,88 @@
-using ClinicaSalud.Models;
 
-//var id = id;
-//string name = name;
+bool stop = true;
 
-/*Console.Write(new Pacientes(123,"szapatar",12, "diarrea").Name);
-Console.Write(new Pacientes(123,"szapatar",12, "diarrea").Id);
-Console.Write(new Pacientes(123,"szapatar",12, "diarrea").Edad);
-Console.Write(new Pacientes(123,"szapatar",12, "diarrea").Sintoma);*/
-bool system = false;
+
 
 List<string> nombrePacientes = new List<string>();
 
-bool exit = false;
-bool repeat = false;
 
 
-{
-    string Options =  "Please Enter The Number Do You Want\n" +
+    string Options =
                       "1.Register Patient\n"+
                      "2. List Patients\n" +
                      "3. Search Patients\n" +
                      "4.Exit\n";
     Console.WriteLine(Options);
+     
 
 
-    ConsoleKeyInfo choose = new ConsoleKeyInfo();
+     while (stop)
+     {
+         
+         Console.WriteLine("Please Enter The Number Do You Want\n"); 
+         Console.WriteLine(Options);
+         string choose = Console.ReadLine();
+         
+         Console.Clear();
 
-    choose = Console.ReadKey(true);
+         switch (choose)
+         {
+             case "1":
+             {
+                 Console.WriteLine("Enter Data Of Patient");
+                 string paciente = Console.ReadLine();
+                 nombrePacientes.Add(paciente);
+                 //Console.Clear();
+                 //Console.WriteLine(Options);
+                 break;
+             }
+
+             case "2":
+             {
+                 foreach (var pacientes in nombrePacientes)
+                 {
+                     Console.WriteLine($"El paciente {pacientes} Esta Registrado");
+                 }
+
+                 Console.ReadLine();
+                 break;
+             }
+
+             case "3":
+             {
+                 Console.Write("Search It \n");
+                 string s = Console.ReadLine();
+
+                 
+                 string Name = nombrePacientes.FirstOrDefault(p =>p == s);
+                 if (Name == null)
+                 {
+                     Console.WriteLine($"The {Name}Is Not In System");
+                     Console.ReadLine();
+                 }
+                 else
+                 {
+                     Console.WriteLine($"The {Name} Is In The System");
+                     Console.ReadLine();
+                 }
+                 
+                 break;
+             }
+
+             case "4":
+             {
+                 stop = false;
+                 break;
+             }
+
+         }
+     }
 
 
-    switch (choose.Key )
-    {
-        case ConsoleKey.D1:
-        {
-            Console.WriteLine("Enter Data Of Patient");
-            string paciente = Console.ReadLine();
-            nombrePacientes.Add(paciente);
-            Console.Clear();
-            Console.WriteLine(Options);
-            repeat = true;
-            break;
-        }
-
-        case ConsoleKey.D2:
-        {
-            foreach (var pacientes in nombrePacientes)
-            {
-                Console.WriteLine($"El paciente {pacientes}");
-            }
-            Console.ReadLine();
-            break;
-        }
-
-        case ConsoleKey.D3:
-        {
-            Console.Write("Do You Want Search Any Patient And\n");
-            break;
-        }
-
-        case ConsoleKey.D4:
-        {
-            Console.Write("Do You Want Exit Of The System And\n");
-            exit = true;
-            break;
-        }
-        
-    }
-    
-}
 
 
 
-   
+
 
 
 
--- obj/Debug/net9.0/ClinicaSalud.AssemblyInfo.cs
+++ obj/Debug/net9.0/ClinicaSalud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicaSalud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a05a30dd61baf5fed77a4aea1412e47db634d960")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1993e5325e60ac155c05c73c6b163f7c163cc3ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicaSalud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicaSalud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
